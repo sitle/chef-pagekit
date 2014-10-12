efbd6b5a9006bb84c129b28a755cc425b6fe1049
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-10-12 03:05:23 -1000

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-pagekit'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-pagekit'
+long_description 'Installs/Configures chef-pagekit'
+version          '0.1.0'
+
